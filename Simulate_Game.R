b9225470284039d2b2f9ddc8c17663f55ec6450e
ManuelVU <jesus_mvu@hotmail.com>
2018-02-08 16:11:00 -0600

    Actualizacion de believes_errors y funcion de simulacion rudimentaria

--- Simulate_Game.R
+++ Simulate_Game.R
@@ -1,3 +1,42 @@
-simulate_game<-function(matrixRow,matrixCol,pairs,trials,lambda){
+# Pairs = How many pairs to be simulated for each value of lambda
+simulate_game<-function(matrixRow,matrixCol,pairs,trials,lambda,belives_row,belives_col){
+  library(nleqslv)
+  belief_error <- function(sigma){
+    expected_payoff1 <- matrixRow%*%sigma[1:ncol(matrixRow)]*lambda[l,1]
+    expected_payoff2 <- matrixCol_t%*%sigma[(ncol(matrixRow)+1):((ncol(matrixRow))+nrow(matrixRow))]*lambda[l,2]
+    SBR <- {}
+    SBR[(ncol(matrixRow)+1):((ncol(matrixRow))+nrow(matrixRow))] <- exp(expected_payoff1)/(sum(exp(expected_payoff1)))
+    SBR[1:ncol(matrixRow)] <- exp(expected_payoff2)/(sum(exp(expected_payoff2)))
+    return(SBR-sigma)
+  }
+  choice_r<-{}
+  choice_c<-{}
+  matrixCol<-matrixCol
+  matrixRow<-matrixRow
+  matrixCol_t<-t(matrixCol)
+  n_p<-pairs
+  t<-trials
+  if(missing(belives_row)){
+    belives_row<-rep(1/nrow(matrixRow),nrow(matrixRow))
+  }
+  if(missing(belives_col)){
+    belives_col<-rep(1/ncol(matrixRow),ncol(matrixRow))
+  }
+  belives<-c(belives_row,belives_col)
+  if(is.vector(lambda)){
+    lambda<-cbind(c(lambda),c(lambda))
+  }
+  for(l in 1:length(lambda[,1])){
+    solution <-nleqslv(fn = belief_error,x=as.vector(belives))
+    choice_r<-rbind(choice_r,t(rmultinom(n_p,t,solution$x[(ncol(matrixRow)+1):(ncol(matrixRow)+nrow(matrixRow))])))
+    choice_c<-rbind(choice_c,
+      t(rmultinom(n_p,t,solution$x[1:ncol(matrixRow)])))
+  }
+  Results<-list()
+  Results$row<-choice_r
+  Results$col<-choice_c
+  return(Results)
   #under construction
 }
+datanew<-simulate_game(matrix(c(17,-1,1,-2,1,-1),nrow=3),-matrix(c(17,-1,1,-2,1,-1),nrow=3),
+                       10,100,10,c(0.2,0.8),c(0.7,0.1,0.2))
--- believes_errors.R
+++ believes_errors.R
@@ -26,12 +26,15 @@ believes <- matrix(c(1/3,2/3,1/2,1/2),nrow=2)
 sigma <- as.vector(believes)
 lambda= 100
 
+m1<-matrix(c(17,-1,1,-2,1,-1),nrow=3)
+m2<--t(m1)
+believes<-c(c(0.2,0.8),c(0.7,0.1,0.2))
 belief_error <- function(sigma){
-  expected_payoff1 <- m1%*%sigma[1:n]*lambda
-  expected_payoff2 <- m2%*%sigma[(n+1):(n*2)]*lambda
+  expected_payoff1 <- m1%*%sigma[1:ncol(m1)]*lambda
+  expected_payoff2 <- m2%*%sigma[(ncol(m1)+1):((ncol(m1))+(nrow(m1)))]*lambda
   SBR <- {}
-  SBR[(n+1):(n*2)] <- exp(expected_payoff1)/(sum(exp(expected_payoff1)))
-  SBR[1:n] <- exp(expected_payoff2)/(sum(exp(expected_payoff2)))
+  SBR[(ncol(m1)+1):((ncol(m1))+(nrow(m1)))] <- exp(expected_payoff1)/(sum(exp(expected_payoff1)))
+  SBR[1:ncol(m1)] <- exp(expected_payoff2)/(sum(exp(expected_payoff2)))
   return(SBR-sigma)
 }
 
