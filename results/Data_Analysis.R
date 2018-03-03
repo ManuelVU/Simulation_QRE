621383671cee641faaf429cc03891d3031390202
ManuelVU <jesus_mvu@hotmail.com>
2018-03-03 15:34:57 -0600

    games

--- results/Data_Analysis.R
+++ results/Data_Analysis.R
@@ -1,15 +1,14 @@
 # This script is to be used only for the main data analysis of the paper on simulation and QRE
-source("src/Functions.R") 
+source("src/Functions.R")
+source("src/Games.R")
 #### Game 1: Battle of the Sexes ####
 Game_id<-"BS"
 #Output path
 OutPath<- "results/"
-g_row<-matrix(c(3,0,0,2),nrow=2)
-g_col<-matrix(c(2,0,0,3),nrow=2)
 players<-50
 t<-150
 l<-2
-belives_r<-rep(1/nrow(g_row),nrow(g_row))
+belives_r<-rep(1/nrow(Games$Sexes$matrixRow),nrow(Games$Sexes$matrixCol))
 belives_c<-rep(1/ncol(g_col),nrow(g_col))
 battle_of_sexes<-simulate_game(matrixRow = g_row,matrixCol = g_col,pairs = players,trials = t,
                                lambda = l,belives_row = belives_r,belives_col = belives_c)
--- src/Games.R
+++ src/Games.R
@@ -9,8 +9,7 @@ m1 <- m1*5
 m2 <- m1*(-1)
 m1 <- Estandar_payoffs(m1)
 m2 <- Estandar_payoffs(m2)
-Oneil <- list(matrixRow=m1,matrixCol=m2) 
-Games$Oneil <- Oneil
+Games$Oneil <- list(matrixRow=m1,matrixCol=m2) 
 
 ## tesis lic dario ####
 n=2
@@ -18,8 +17,7 @@ m1 <- matrix(c(-9,1,9,-1),nrow=n)
 m2 <- matrix(c(9,-9,-1,1),nrow=n)
 m1 <- Estandar_payoffs(m1)
 m2 <- Estandar_payoffs(m2)
-Dario <- list(matrixRow=m1,matrixCol=m2) 
-Games$Dario <- Dario
+Games$Dario <- list(matrixRow=m1,matrixCol=m2) 
 
 #Coordination: sexos ####
 n=2
@@ -27,17 +25,15 @@ m1<-matrix(c(2,0,0,1),nrow=n)
 m2<-matrix(c(1,0,0,2),nrow=n)
 m1 <- Estandar_payoffs(m1)
 m2 <- Estandar_payoffs(m2)
-Sexes <- list(matrixRow=m1,matrixCol=m2) 
-Games$Sexes <- Sexes
+Games$Sexes <- list(matrixRow=m1,matrixCol=m2) 
 
 # asimetrico ####
 n=2
-m1 <- matrix(c(2,-2,-1,1),nrow=n)
-m2 <- -t(m1)
+m1 <- matrix(c(9,0,0,1),nrow=n)
+m2 <- matrix(c(0,4,4,0))
 m1 <- Estandar_payoffs(m1)
 m2 <- Estandar_payoffs(m2)
-Asymmetric <- list(matrixRow=m1,matrixCol=m2) 
-Games$Asymmetric <- Asymmetric
+Games$Asymmetric <- list(matrixRow=m1,matrixCol=m2) 
 
 # Prisioner's Dilemma ####
 n=2
@@ -45,6 +41,5 @@ m1 <- matrix(c(-1,0,-3,-2),nrow=n)
 m2 <- t(m1)
 m1 <- Estandar_payoffs(m1)
 m2 <- Estandar_payoffs(m2)
-PDilemma <- list(matrixRow=m1,matrixCol=m2) 
-Games$PDilemma <- PDilemma
-
+Games$PDilemma <- list(matrixRow=m1,matrixCol=m2) 
+rm(list=c("m1","m2","n"))
